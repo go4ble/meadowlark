48f2de5542c27385c6e5a29d5455f0112a0413e2
Kyle Boone <go4ble23@gmail.com>
2016-01-16 15:41:13 -0800

    views and fortunes

--- meadowlark.js
+++ meadowlark.js
@@ -1,39 +1,34 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    //res.type('text/plain');
-    //res.send('Meadowlark Travel');
     res.render('home');
 });
 
 app.get('/about', function(req, res) {
-    //res.type('text/plain');
-    //res.send('About Meadowlark Travel');
-    res.render('home');
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 // custom 404 page
 app.use(function(req, res, next) {
-    //res.type('text/plain');
     res.status(404);
-    //res.send('404 - Not Found');
     res.render('404');
 });
 
 // custom 500 page
 app.use(function(err, req, res, next) {
     console.error(err.stack);
-    //res.type('text/plain');
     res.status(500);
-    //res.send('500 - Server Error');
     res.render('500');
 });
 
